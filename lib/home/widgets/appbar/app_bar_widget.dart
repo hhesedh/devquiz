8f7c227e40389d90936a57cfcbd9ca7226e7d211
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2021-04-25 00:57:37 -0300

    criação da app bar

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,44 @@
+import 'package:devquiz/core/core.dart';
+import 'package:flutter/widgets.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            decoration: BoxDecoration(
+              gradient: AppGradients.linear,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                        text: "Olá, ",
+                        style: AppTextStyles.title,
+                        children: [
+                          TextSpan(
+                              text: "Fábio DEV",
+                              style: AppTextStyles.titleBold),
+                        ]),
+                  ),
+                  Container(
+                    width: 58,
+                    height: 58,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://avatars.githubusercontent.com/u/35357679?v=4'),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+}
